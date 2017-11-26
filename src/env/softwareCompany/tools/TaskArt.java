a7ae46bdae42773baed1e44140844f6b2dfdfafe
tmlima <thiago.machado.lima@gmail.com>
2017-11-26 18:25:41 -0200

    Refactoring

--- src/env/softwareCompany/tools/TaskArt.java
+++ src/env/softwareCompany/tools/TaskArt.java
@@ -4,15 +4,15 @@ import cartago.*;
 
 public class TaskArt extends Artifact {
 			
-	private static final String PHASE_OBS_PROPERTY_ID = "id";	
-	private static final String PHASE_OBS_PROPERTY_NAME = "phase";
-	private static final String SIZE_PROPERTY_NAME = "size";
+	private static final String PROPERTY_NAME_ID = "id";	
+	private static final String PROPERTY_NAME_PHASE_OBS = "phase";
+	private static final String PROPERTY_NAME_SIZE = "size";
 	private static int lastTaskId = 1;
 	
 	void init(String name, String size) {
-        defineObsProperty(PHASE_OBS_PROPERTY_ID, lastTaskId);
-        defineObsProperty(PHASE_OBS_PROPERTY_NAME, RequirementPhase.DEV);
-        defineObsProperty(SIZE_PROPERTY_NAME, size);
+        defineObsProperty(PROPERTY_NAME_ID, lastTaskId);
+        defineObsProperty(PROPERTY_NAME_PHASE_OBS, RequirementPhase.DEV);
+        defineObsProperty(PROPERTY_NAME_SIZE, size);
 		lastTaskId++;
 	}
 		
@@ -28,11 +28,11 @@ public class TaskArt extends Artifact {
 	
 	@OPERATION
 	void size(OpFeedbackParam size) {
-		size.set(getObsProperty(SIZE_PROPERTY_NAME).getValue());
+		size.set(getObsProperty(PROPERTY_NAME_SIZE).getValue());
 	}
 	
 	private void updatePhase(RequirementPhase phase) {
-		ObsProperty prop = getObsProperty(PHASE_OBS_PROPERTY_NAME);
+		ObsProperty prop = getObsProperty(PROPERTY_NAME_PHASE_OBS);
 		prop.updateValue(phase);
 	}
 }
